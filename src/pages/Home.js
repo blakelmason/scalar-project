89d60f2094f8a14422b257d06fcb6df44c882e54
blake <blakelmason@gmail.com>
2020-08-27 17:12:51 -0700

    alt tag

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
         style={{ maxWidth: 500 }}
         className="img-fluid rounded shadow border mb-4"
         src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg"
-        alt="cat picture"
+        alt="Lounging Cat"
       />
       <p>I hope you enjoy it as much as this cat enjoys lounging.</p>
     </div>
